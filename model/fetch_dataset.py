b7dbe36f29116f90e6e24abb17b482acc61ef74d
Carlos <carlos.pinavazgomes@student.manchester.ac.uk>
2018-10-20 01:31:33 +0200

    Add code to fetch dataset with pandas and return titles and bodies

---
+++ model/fetch_dataset.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+
+def read_dataset():
+    dataset = pd.read_csv("data/train.csv")
+    dataset = dataset.drop("id", axis=1)
+    print(dataset.columns)
+    dataset["text"] = dataset["text"].str.replace("\n", "")
+
+    titles = [title for title in dataset["title"]]
+
+    bodies = [body for body in dataset["text"]]
+
+    return (titles, bodies)
