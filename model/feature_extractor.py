4862427159dc0e9d6e364a6bab8195ede557aeb4
Andrej Ivanov <ivandrej@gmail.com>
2018-10-20 08:17:42 +0100

    Pickle title and body vectorizer separately

--- model/feature_extractor.py
+++ model/feature_extractor.py
@@ -1,42 +1,43 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pickle
 from os import path
+from enum import Enum
 
 
-tfidf_pickle_path = 'pickles/vectorizer.sav'
+tfidf_pickle_path = 'pickles'
+
+class Text(Enum):
+    TITLE = 1
+    BODY = 2
 
 
-def fill_it_up_real_nice(x):
-    return x.todense()
+def get_full_pickle_path(text_type):
+    return path.join(tfidf_pickle_path, text_type.name + '_vectorizer.sav')
 
 
-def fit_tf_idf(texts_dataset):
-    # vectorizer = TfidfVectorizer()
-    # vectorizer.fit(corpus)
-    # pickle.dump(vectorizer, open(tfidf_pickle_path, 'wb'))
+def fit_tf_idf(texts_dataset, text_type):
+    vectorizer = TfidfVectorizer()
+    vectorizer.fit(texts_dataset)
+    pickle.dump(vectorizer, open(get_full_pickle_path(text_type), 'wb'))
     
-    # return vectorizer
-    pass
+    return vectorizer
 
 
-def extract_tf_idf(texts_dataset, doIFillItUpRealNice=False):
+def extract_tf_idf(texts_dataset, text_type=Text.BODY):
     # for testing, use vectorizer created during training
-    if path.exists(tfidf_pickle_path):
-        vectorizer = pickle.load(open(tfidf_pickle_path, 'rb'))
+    if path.exists(get_full_pickle_path(text_type)):
+        vectorizer = pickle.load(open(get_full_pickle_path(text_type), 'rb'))
     # for training, create new vectorizer
     else:
-        vectorizer = TfidfVectorizer()
-        vectorizer.fit(texts_dataset)
-        pickle.dump(vectorizer, open(tfidf_pickle_path, 'wb'))
+        vectorizer = fit_tf_idf(texts_dataset, text_type)
 
     X = vectorizer.transform(texts_dataset)
-    if doIFillItUpRealNice:
-        X = X.todense()
 
     return X
 
 
 def extract_features(titles, bodies):
     bodies_tfidf = extract_tf_idf(bodies)
+    titles_tfidf = extract_tf_idf(titles, text_type=Text.TITLE)
 
     return bodies_tfidf
