be1baf27bcee7ec60946752ceb6f7b3a6130584c
Carlos Gomes <carlosmiguel.gomes@live.com.pt>
2018-10-20 21:49:47 +0200

    Fix port issues

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from article_parser.article_parser import ArticleParser
 from main import rate
+import os
 
 class fake_o_meter(Resource):
     def post(self):
@@ -27,4 +28,5 @@ api = Api(app)
 api.add_resource(fake_o_meter, '/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, port=port)
