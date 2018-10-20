f2a93d0cea03fb7cd07edac261d14c19f24aaaf9
Andrej Ivanov <ivandrej@gmail.com>
2018-10-20 07:56:31 +0100

    Clean up carlos's shit code

--- model/Classifier.py
+++ model/Classifier.py
@@ -4,26 +4,28 @@ from sklearn.metrics import accuracy_score
 import feature_extractor
 import fetch_dataset
 import pickle
+
 # pandas dataframe containing title, text and label(0 for true 1 for fake)
 dataset = fetch_dataset.read_dataset("data/train.csv")
 train_set, test_set = train_test_split(dataset, test_size=0.2)
 
-train_titles = train_set[:, 0]
-train_bodies = train_set[:, 1]
+train_titles = train_set[:, 0].tolist()
+train_bodies = train_set[:, 1].tolist()
 Y_train = train_set[:, 2]
 Y_train = Y_train.astype('int')
-test_titles = test_set[:, 0]
-test_bodies = test_set[:, 1]
-Y_test = test_set[:, 2]
+test_titles = test_set[:, 0].tolist()
+test_bodies = test_set[:, 1].tolist()
+Y_test = test_set[:, 2].astype('int')
 
-X_train = feature_extractor.extract_features(train_titles, train_bodies.tolist())
+X_train = feature_extractor.extract_features(train_titles, train_bodies)
 
-print(Y_train.shape)
-print(X_train.shape)
+print(X_train.shape, Y_train.shape)
 reg = linear_model.LogisticRegression(verbose=1)
 reg.fit(X_train, Y_train)
 
-print(accuracy_score(reg.predict(feature_extractor.extract_features(test_titles, test_bodies.tolist())), Y_test.astype("int")))
+X_test = feature_extractor.extract_features(test_titles, test_bodies)
+
+print(accuracy_score(reg.predict(X_test), Y_test))
 
 pickle.dump(reg, open("pickles/regression.sav", 'wb'))
 
