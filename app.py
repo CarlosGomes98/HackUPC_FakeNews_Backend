ec022674d9749dbe8e092528b4ee8a1fcb57054c
Carlos Gomes <carlosmiguel.gomes@live.com.pt>
2018-10-21 07:32:54 +0200

    Change host back

--- app.py
+++ app.py
@@ -28,4 +28,4 @@ api.add_resource(fake_o_meter, '/')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='127.0.0.1', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
